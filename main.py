9183cd4068d6f2ea1bc4c559484c3e48d03efa82
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2020-12-17 23:50:28 +0100

    parece que funciona

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import requests
 
-direccion = "http://www.omdbapi.com/?apikey=e45ea178&i=tt3896198"
+pregunta = input("Título de la película")
+API_KEY = "e45ea178"
+direccion = f"http://www.omdbapi.com/?apikey={API_KEY}&s={pregunta}"
 
 respuesta = requests.get(direccion)
+
 if respuesta.status_code == 200:
     datos = respuesta.json()
-    print (datos.keys())
-else:
-    print ("Se ha producido un error", respuesta.status_code)
+    if datos["Response"] == "False":
+        print (datos["Error"])
+    else:
+        primera_peli = datos['Search'][0]
+        clave = primera_peli['imdbID']
+
+        otra_direccion = "http://www.omdbapi.com/?apikey={}&i={}".format(API_KEY, clave)
+        nueva_respuesta = requests.get(otra_direccion)
+        if nueva_respuesta.status_code == 200:
+            datos = nueva_respuesta.json()
+            if datos["Response"] == "False":
+                print (datos["Error"])
+            else:
+                titulo = datos['Title']
+                agno = datos['Year']
+                director = datos['Director']
+                print ("La peli {}, estrenada en el año {}, fue dirigida por {}".format(titulo, agno, director))
+        else:
+            print ("Error en consulta por ID", nueva_respuesta.status_code)
+
+else: 
+    print ("Error en consulta en búsqueda", respuesta.status_code)
+
+
